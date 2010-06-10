038555603d7b361e4f7461b256d48b345d31a6fc
lyse damasked <lyase.d@gmail.com>
2010-06-10 14:00:27 +0200

    added documentation generation structure and samples

--- test/testPass.py
+++ test/testPass.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python
+##\file testPass.py
+#\brief en bref:Programme de tests.
+#\author lyase D.
+#\version 0.1
+#\date 10 Juin 2010.\n
+# plus d'info sur le fichier ci present.
+## @package testPass.py
+# this module is the central repository of our tests.
+# each class is a new world.
+# each methode is a test in that world.
+# think use cases.
+
 import random
 import unittest
+## this class will hold all the test cases of the app it will act as the test runner\n 
+# and doc generator seed.
+#each test will be written in a function starting by keywork test \n
+# and it's purpose described in embeded doc.
+# before the actual code as shown here.
 
 class TestSequenceFunctions(unittest.TestCase):
 
+## the setUp is rerun before each test.
+#more doc on setup.
     def setUp(self):
-#this will be run before each test i think
+
         self.seq = range(10)
+#--------------------------------doc of testPass -----------------------------------------
+
+## this method is test is the hello world of tests it will always pass.
+    #  @param self The object pointer.
+        # make sure the shuffled sequence does not lose any elements
 
     def testPass(self):
+
+    #  @param self The object pointer.
         # make sure the shuffled sequence does not lose any elements
+
         self.assertEqual(True,True)
 if __name__ == '__main__':
         unittest.main()
